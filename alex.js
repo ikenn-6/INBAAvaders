415d8464d4534448b6de59f377e552df74da542f
UTILISATEUR <UTILISATEUR@192.168.1.165>
2017-07-27 13:42:36 +0200

    Test Tir
    Pour tester le mécanisme de tir et le fonctionnement des commit

--- alex.js
+++ alex.js
@@ -1,3 +1,37 @@
-/**
- * 
- */
+document.addEventListener("keydown", tir, false);
+
+function tir(e) 
+{
+  if(e.keyCode==32) 
+  {
+	  var projectile = document.createElement("DIV");
+	  projectile.classList.add("projectile");
+	  	  
+	  document.getElementById("testTir").appendChild(projectile); 
+	  
+	  //recuperer la position du joueur
+	  //en attendant la position est en dur dans la classe
+	  document.getElementsByClassName("projectile").item(0).style.top = "300px"; //a changer!
+	 // document.getElementsByClassName("projectile").item(1).style.top = "300px"; //a changer!
+	  
+	  setInterval(mouvementProjectile, 500);
+  } 
+}
+
+function mouvementProjectile()
+{
+	var projectiles = document.getElementsByClassName("projectile");
+	for(var i = 0; i< projectiles.length; i++)
+	{
+		//alert(projectiles);
+		//alert(parseInt(document.getElementsByClassName("projectile").item(0).style.top));
+		currentPosition = parseInt(projectiles.item(i).style.top);
+		projectiles.item(i).style.top = currentPosition - 10 + "px";
+	}
+
+}
+
+function stopMouvementProjectile()
+{
+	
+}
