68c432381054538bcca357cd5e26275fd614f339
UTILISATEUR <UTILISATEUR@192.168.1.165>
2017-07-27 15:59:30 +0200

    modification tir

--- alex.js
+++ alex.js
@@ -1,23 +1,29 @@
 document.addEventListener("keydown", tir, false);
+var dernierTir = new Date();
 
 function tir(e) 
 {
-  if(e.keyCode==32) 
-  {
-	  var projectile = document.createElement("DIV");
-	  projectile.classList.add("projectile");
-	  projectile.style.top = "300px";
+	
+	var currentTime = new Date();
+	//console.log(currentTime);
 
-	  
-	 // var milieuJoueur = parseInt(document.getElementById("joueur").style.left) + (parseInt(document.getElementById("joueur").style.width)) + "px";
-	  
-	  var milieuJoueur = "150px";
-	  projectile.style.left = milieuJoueur;
-	  document.getElementById("testTir").appendChild(projectile); 
-	  
-	  
-	  setInterval(mouvementProjectile, 500);
-  } 
+	//alert(dernierTir);
+	if(currentTime - dernierTir > 1000 && e.keyCode==32)
+	{		
+		console.log(currentTime);
+			dernierTir = new Date();
+			var projectile = document.createElement("DIV");
+			projectile.classList.add("projectile");
+			projectile.style.top = "300px";
+
+			// var milieuJoueur = parseInt(document.getElementById("joueur").style.left) + (parseInt(document.getElementById("joueur").style.width)) + "px";
+			//A remplacer
+			var milieuJoueur = "150px";
+			projectile.style.left = milieuJoueur;
+			document.getElementById("testTir").appendChild(projectile); 
+		  
+			setInterval(mouvementProjectile, 500);
+	}
 }
 
 function mouvementProjectile()
