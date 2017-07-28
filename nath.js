a9ef0ffb8ccde2c7d07936b93ea2edd449ff9641
UTILISATEUR <UTILISATEUR@192.168.1.165>
2017-07-28 13:15:40 +0200

    testttt

--- nath.js
+++ nath.js
@@ -1,5 +1,5 @@
 
-    var stop = setInterval(shiftingextra, 100);
+    var stop = setInterval(shiftingextra2, 100);
     var directionalien = true;
     
 function shiftingextra(){
@@ -25,6 +25,39 @@ function shiftingextra(){
     }
 }
 
+function shiftingextra2(){
+    var leftalien = parseInt(document.getElementById("alien").style.left);
+    var topalien = parseInt(document.getElementById("alien").style.top);
+        for(i=0; i<55; i++){
+    if(leftalien >= 75){
+        document.getElementById("alien").style.top = topalien + 5 + "px";
+        directionalien = false;
+    }
+    if(leftalien <= 0){
+        document.getElementById("alien").style.top = topalien + 5 + "px";
+        directionalien = true;
+    }
+    if(directionalien == false){
+        document.getElementById("alien").style.left = leftalien - 1 + "px";
+    }
+    if(directionalien == true ){
+        document.getElementById("alien").style.left = leftalien + 1 + "px";
+    }
+    if(topalien >= 200){
+        clearInterval(stop);
+    }
+        }
+}
+
+
+
+
+
+
+
+
+
+
 function load(){
 	//Positionnement du bloc des aliens
     document.getElementById("alien").style.left = "0px";
@@ -48,33 +81,39 @@ function load(){
             	listeImages[i].setAttribute("id", "alien"+i);
         		currentLeft = 0;
         		currentTop = 0;
+        		listeImages[i].setAttribute("position", "absolute");
         	}
         	if(i == 11)
         	{
             	listeImages[i].setAttribute("id", "alien"+i);
         		currentLeft = 0;
         		currentTop = 55;
+        		listeImages[i].setAttribute("position", "absolute");
         	}
         	if(i == 22)
         	{
             	listeImages[i].setAttribute("id", "alien"+i);
         		currentLeft = 0;
         		currentTop = 110;
+        		listeImages[i].setAttribute("position", "absolute");
         	}
         	if(i == 33)
         	{
             	listeImages[i].setAttribute("id", "alien"+i);
         		currentLeft = 0;
         		currentTop = 165;
+        		listeImages[i].setAttribute("position", "absolute");
         	}
         	if(i == 44)
         	{
             	listeImages[i].setAttribute("id", "alien"+i);
         		currentLeft = 0;
         		currentTop = 220;
+        		listeImages[i].setAttribute("position", "absolute");
+        		
         	}
-        	listeImages[i].setAttribute("left", currentLeft);
-        	listeImages[i].setAttribute("top", currentTop);
+        	//listeImages[i].setAttribute("left", currentLeft);
+        	//listeImages[i].setAttribute("top", currentTop);
         	//listeImages[i].style.left= currentLeft + "px";
         	//listeImages[i].style.top= currentTop + "px";
     	}
@@ -84,6 +123,7 @@ function load(){
         	listeImages[i].setAttribute("id", "alien"+i);
         	listeImages[i].setAttribute("left", currentLeft);
         	listeImages[i].setAttribute("top", 0);
+        	listeImages[i].setAttribute("position", "absolute");
     	}
     	if(i> 11 && i<=21)
     	{
@@ -91,6 +131,7 @@ function load(){
         	listeImages[i].setAttribute("id", "alien"+i);
         	listeImages[i].setAttribute("left", currentLeft);
         	listeImages[i].setAttribute("top", 55);
+        	listeImages[i].setAttribute("position", "absolute");
     	}
     	if(i> 22 && i<=32)
     	{
@@ -98,6 +139,7 @@ function load(){
         	listeImages[i].setAttribute("id", "alien"+i);
         	listeImages[i].setAttribute("left", currentLeft);
         	listeImages[i].setAttribute("top", 110);
+        	listeImages[i].setAttribute("position", "absolute");
     	}
     	if(i> 33 && i<=43)
     	{
@@ -105,6 +147,7 @@ function load(){
         	listeImages[i].setAttribute("id", "alien"+i);
         	listeImages[i].setAttribute("left", currentLeft);
         	listeImages[i].setAttribute("top", 165);
+        	listeImages[i].setAttribute("position", "absolute");
     	}
     	if(i> 44 && i<=54)
     	{
@@ -112,6 +155,7 @@ function load(){
         	listeImages[i].setAttribute("id", "alien"+i);
         	listeImages[i].setAttribute("left", currentLeft);
         	listeImages[i].setAttribute("top", 220);
+        	listeImages[i].setAttribute("position", "absolute");
     	}
     }
     
