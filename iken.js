1c28ddae7cde23b61bc890035a1b6b4b616e1ce4
UTILISATEUR <UTILISATEUR@192.168.1.167>
2017-07-28 14:38:56 +0200

    Code Ikenna Nathaniel assemblé !

--- iken.js
+++ iken.js
@@ -1,3 +1,13 @@
-/**
- * 
- */
+function bouger() {
+    var bouger = parseInt(document.getElementById("vaisseau").style.left);
+
+    if (event.keyCode == 39 && bouger <= 690) {
+        document.getElementById("vaisseau").style.left = bouger + 10 + 'px';
+    }
+    if (event.keyCode == 37 && bouger >= 10) {
+        document.getElementById("vaisseau").style.left = bouger - 10 + 'px';
+    }
+}
+function loadi(){
+    document.getElementById("vaisseau").style.left = "0px";
+}
